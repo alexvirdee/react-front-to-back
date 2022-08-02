755c00334a527fe7c9b75753ed7f66c4f8b600b3
Alexinder Virdee <alex.virdee08@gmail.com>
2022-08-02 14:14:17 -0400

    Display users from api

--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
 
 function UserResults() {
   const [users, setUsers] = useState([]);
@@ -22,13 +24,17 @@ function UserResults() {
     setLoading(false);
   };
 
-  return (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  );
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default UserResults;
