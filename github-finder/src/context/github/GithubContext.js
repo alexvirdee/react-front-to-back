ffb629db077979a3bba4688b56e805ba104068ff
Alexinder Virdee <alex.virdee08@gmail.com>
2022-08-16 11:51:29 -0400

    Move getUser to actions file

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -3,9 +3,6 @@ import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
@@ -14,78 +11,14 @@ export const GithubProvider = ({ children }) => {
     loading: false,
   };
 
-  // Set Loading
-  const setLoading = () =>
-    dispatch({
-      type: 'SET_LOADING',
-    });
-
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-
-
-
-  // Get a single user
-  const getUser = async (login) => {
-    setLoading();
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    if (response.status === 404) {
-      window.location = '/notfound';
-    } else {
-      const data = await response.json();
-
-      dispatch({
-        type: 'GET_USER',
-        payload: data,
-      });
-    }
-  };
-
-   // Get user repositories
-   const getUserRepos = async (login) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      sort: 'created',
-      per_page: 10
-    })
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await response.json();
-
-    dispatch({
-      type: 'GET_REPOS',
-      payload: data
-    });
-  };
-
-  const clearUsers = () => {
-    dispatch({
-      type: 'CLEAR_USERS',
-    });
-  };
 
   return (
     <GithubContext.Provider
       value={{
         ...state,
         dispatch,
-        clearUsers,
-        getUser,
-        getUserRepos
       }}
     >
       {children}
