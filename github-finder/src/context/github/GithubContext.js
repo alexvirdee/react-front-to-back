fb1504065e3ee6fcdcca461f65d09d169aa39fee
Alexinder Virdee <alex.virdee08@gmail.com>
2022-08-16 11:42:46 -0400

    Move searchUsers to actions file

--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
 
@@ -21,28 +22,8 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get search results
-  const searchUsers = async (text) => {
-    setLoading();
 
-    const params = new URLSearchParams({
-      q: text,
-    });
 
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    const { items } = await response.json();
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    });
-  };
 
   // Get a single user
   const getUser = async (login) => {
@@ -67,6 +48,30 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+   // Get user repositories
+   const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: 'created',
+      per_page: 10
+    })
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await response.json();
+
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data
+    });
+  };
+
   const clearUsers = () => {
     dispatch({
       type: 'CLEAR_USERS',
@@ -76,12 +81,11 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        loading: state.loading,
-        searchUsers,
+        ...state,
+        dispatch,
         clearUsers,
         getUser,
+        getUserRepos
       }}
     >
       {children}
